a66cea20d8ef07609a9d255beb73ca144b390978
nbozon <nicolas.bozon@cartogenic.com>
2020-04-18 20:52:36 +0200

    Adds clcik event on primary menu items
    Adds clcik event on primary menu items for screen wider than 768px.  Their behavior stays unchanged on mobile (tap to toggle dropdown, no link available).

--- themes/grass/assets/js/script.js
+++ themes/grass/assets/js/script.js
@@ -8,12 +8,29 @@
         });
     });
 
-
-    // Accordions
-    $('.collapse').on('shown.bs.collapse', function () {
-        $(this).parent().find('.ti-plus').removeClass('ti-plus').addClass('ti-minus');
-    }).on('hidden.bs.collapse', function () {
-        $(this).parent().find('.ti-minus').removeClass('ti-minus').addClass('ti-plus');
+    // Boostrap menu hack
+    $(document).ready(function() {
+      function bindNavbar() {
+        if ($(window).width() > 768) {
+          $('.navbar-default .dropdown').on('mouseover', function() {
+            $('.dropdown-toggle', this).next('.dropdown-menu').show();
+          }).on('mouseout', function() {
+            $('.dropdown-toggle', this).next('.dropdown-menu').hide();
+          });
+          $('.dropdown-toggle').click(function() {
+            if ($(this).next('.dropdown-menu').is(':visible')) {
+              window.location = $(this).attr('href');
+            }
+          });
+        } else {
+          $('.navbar-default .dropdown').off('mouseover').off('mouseout');
+        }
+      }
+      $(window).resize(function() {
+        bindNavbar();
+      });
+      bindNavbar();
     });
 
+    
 })(jQuery);
