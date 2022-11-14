d63c58a70a0adaad53fbce09f7f35da5ba7383cc
daniel torres <nobeeakon@gmail.com>
2022-11-14 08:37:13 -0600

    removed console log

--- themes/grass/static/plugins/tracking/matomo.js
+++ themes/grass/static/plugins/tracking/matomo.js
@@ -8,5 +8,3 @@ _paq.push(['enableLinkTracking']);
   var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
   g.type='text/javascript'; g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
 })();
-
-console.log
