1d8f972b01e72f6226eb12abe735a49273ae66eb
Corey White <ctwhite48@gmail.com>
2025-01-24 16:37:25 -0500

    Fixed download tabs

--- themes/grass/assets/js/script.js
+++ themes/grass/assets/js/script.js
@@ -65,7 +65,6 @@
     function detectOSFromUserAgent() {
       return new Promise((resolve) => {
         const userAgent = window.navigator.userAgent;
-
         if (userAgent.includes("Win")) resolve("Windows");
         else if (userAgent.includes("Mac")) resolve("macOS");
         else if (userAgent.includes("Linux")) resolve("Linux");
@@ -75,46 +74,84 @@
       });
     }
 
+    function configureDownloadButton(os, replaceHash = true) {
+      const button = $(".grass-os-download-button");
+      switch (os) {
+        case "Windows":
+            console.log("Setting Windows");
+            button.text("Download for Windows");
+            button.data("os", "windows");
+            $('#downloadTab a[href="#windows"]').tab('show');
+            if (window.location.pathname === "/learn/download/") {
+              history.replaceState(null, null, "#windows");
+            }
+            break;
+        case "macOS":
+            button.text("Download for macOS");
+            button.data("os", "mac");
+            $('#downloadTab a[href="#mac"]').tab('show');
+            if (window.location.pathname === "/learn/download/") {
+              history.replaceState(null, null, "#mac");
+            }
+            break;
+        case "Linux":
+            button.text("Download for Linux");
+            button.data("os", "linux");
+            $('#downloadTab a[href="#linux"]').tab('show');
+            if (window.location.pathname === "/learn/download/") {
+              history.replaceState(null, null, "#linux");
+            }
+            break;
+        case "Docker":
+            button.text("Docker Container");
+            button.data("os", "docker");
+            $('#downloadTab a[href="#docker"]').tab('show');
+            if (window.location.pathname === "/learn/download/") {
+              history.replaceState(null, null, "#docker");
+            }
+            break;
+        default:
+            button.text("Download");
+            button.data("os", "unknown");
+            break;
+    }
+  }
+
     $(document).ready(function() {
+    
+     // Handle download page tab change
+     $('#downloadTab a').on('click', function(e) {
+        e.preventDefault();
+        console.log("Download button clicked", e, this);
+        // Activate the clicked tab
+        $(this).tab('show');
+        const hash = e.target.hash;
+        const os = e.target.innerText;
+        configureDownloadButton(os);
+        // history.replaceState(null, null, hash);
+      });
+
       // Detect the user's OS and update the download button text
       (async () => {
-          const os = await detectOSFromUserAgent();
-          const button = $(".grass-os-download-button");
-       
-          // Add text and data based on the user's OS
-          switch (os) {
-              case "Windows":
-                  button.text("Download for Windows");
-                  button.data("os", "windows");
-                  // TODO: This doesn't work
-                  // $('a[data-toggle="tab"]').on("shown.bs.tab", (e) => {
-                  //   history.replaceState(null, null, "#windows");
-                  //   window.location.hash = "#windows";
-                  // });
-                  if (window.location.pathname === "/learn/download/") {
-                    history.replaceState(null, null, "#windows");
-                  }
-                  break;
-              case "macOS":
-                  button.text("Download for macOS");
-                  button.data("os", "mac");
-                  $('#downloadTab a[href="#mac"]').tab('show');
-                  if (window.location.pathname === "/learn/download/") {
-                    history.replaceState(null, null, "#mac");
-                  }
-                  break;
-              case "Linux":
-                  button.text("Download for Linux");
-                  button.data("os", "linux");
-                  if (window.location.pathname === "/learn/download/") {
-                    history.replaceState(null, null, "#linux");
-                  }
-                  break;
-              default:
-                  button.text("Download");
-                  button.data("os", "unknown");
-                  break;
+          let os = "Unknown OS";
+          // If the user has already selected a tab, don't change it
+          const hash = window.location.hash;
+          if (hash) {
+            const tab = $(`#downloadTab a[href="${hash}"]`)
+            console.log("Open Tab", tab);
+            os = tab.text(); // Get the OS from the tab text
+            console.log("Open Tab OS", os);
+
           }
+          else {
+            // No hash, so detect the OS and select the appropriate tab
+            os = await detectOSFromUserAgent();
+            console.log("No Hash", os);
+          }
+          // Configure the download button
+          // Add text and data based on the user's OS
+          configureDownloadButton(os);
+          
       })(jQuery);
   });
   
