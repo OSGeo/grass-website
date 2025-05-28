864a4a9f4b9802ae8509389548486654ac3357e3
Corey White <smortopahri@gmail.com>
2025-05-28 17:56:09 -0400

    console: Change GRASS GIS to GRASS in console message (#545)

--- themes/grass/assets/js/contributor_ad.js
+++ themes/grass/assets/js/contributor_ad.js
@@ -1,14 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
 const header = `
-   __________  ___   __________    _______________
-  / ____/ __ \\/   | / ___/ ___/   / ____/  _/ ___/
- / / __/ /_/ / /| | \\__ \\\\_  \\   / / __ / / \\__ \\
-/ /_/ / _, _/ ___ |___/ /__/ /  / /_/ // / ___/ /
-\\____/_/ |_/_/  |_/____/____/   \\____/___//____/
+   __________  ___   __________ 
+  / ____/ __ \\/   | / ___/ ___/  
+ / / __/ /_/ / /| | \\__ \\\\_  \\
+/ /_/ / _, _/ ___ |___/ /__/ / 
+\\____/_/ |_/_/  |_/____/____/
 
 `;
     console.log(header);
-    const message = "Curious about GRASS GIS? Consider contributing or join our mentoring program!";
+    const message = "Curious about GRASS? Consider contributing or join our mentoring program!";
     console.log(message);
     const link = "https://grass.osgeo.org/contribute/development/";
     console.log(link);
